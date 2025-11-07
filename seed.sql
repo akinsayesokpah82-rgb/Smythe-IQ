d4217359f0448c8e6c361372ba45384262ce7729
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-07 08:13:05 +0000

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO courses (title, description, level) VALUES
+  ('Fundamentals of Nursing (Freshman 1)', 'Introductory nursing course covering basics', 'freshman'),
+  ('Anatomy & Physiology', 'Basic human anatomy and physiology', 'freshman');
+
+INSERT INTO surveys (title, reward_cents) VALUES
+  ('Health habits survey', 50),
+  ('Learning preferences', 75);
